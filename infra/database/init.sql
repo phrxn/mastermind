e2c100937ca0a26731e0261ec54978a9a7887d0d
phrxn <david@quazzom.com>
2026-03-21 00:31:21 -0300

    feat(infra): update the table design in the database
    - Decrease the size of the 'secret_code' column in the 'games' table
    - Decrease the size of the 'guess' column in the 'guesses' table

--- infra/database/init.sql
+++ infra/database/init.sql
@@ -6,10 +6,6 @@ CREATE TABLE users (
     age TINYINT UNSIGNED NOT NULL CHECK (age BETWEEN 1 AND 120),
     password VARCHAR(255) NOT NULL,
     login_attempts TINYINT UNSIGNED DEFAULT 0 CHECK (login_attempts <= 100),
-    best_score_easy INT DEFAULT 0,
-    best_score_normal INT DEFAULT 0,
-    best_score_hard INT DEFAULT 0,
-    best_score_mastermind INT DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -19,10 +15,9 @@ CREATE TABLE games (
     level ENUM('EASY', 'NORMAL', 'HARD', 'MASTERMIND') NOT NULL,
     code_length TINYINT NOT NULL,
     allow_duplicates BOOLEAN NOT NULL,
-    secret_code VARCHAR(50) NOT NULL,
+    secret_code VARCHAR(18) NOT NULL,
     status ENUM('IN_PROGRESS', 'WON', 'LOST', 'GAVE_UP') NOT NULL,
     attempts_used TINYINT DEFAULT 0,
-    score INT DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     finished_at TIMESTAMP NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
@@ -32,7 +27,7 @@ CREATE TABLE guesses (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     game_id BIGINT NOT NULL,
     attempt_number TINYINT NOT NULL,
-    guess VARCHAR(50) NOT NULL,
+    guess VARCHAR(18) NOT NULL,
     correct_positions TINYINT NOT NULL,
     correct_colors TINYINT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
